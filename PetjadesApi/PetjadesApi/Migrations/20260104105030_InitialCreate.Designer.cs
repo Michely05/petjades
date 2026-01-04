bab7a64bb4ad4770d80d0edc21039d7531a0b33d
[Michely Paredes Galeano] <[michelyparedes@gmail.com]>
2026-01-04 11:51:20 +0100

    add migrations for railway

--- PetjadesApi/PetjadesApi/Migrations/20260104105030_InitialCreate.Designer.cs
+++ PetjadesApi/PetjadesApi/Migrations/20260104105030_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using PetjadesApi;
 namespace PetjadesApi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260104102431_InitialCreate")]
+    [Migration("20260104105030_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
