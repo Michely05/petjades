5b6158fa451df1027982e0b101d9052301b3d107
[Michely Paredes Galeano] <[michelyparedes@gmail.com]>
2026-01-04 14:29:24 +0100

    add Cloudinary service to upload images

---
+++ PetjadesApi/PetjadesApi/Services/CloudinaryService.cs
@@ -0,0 +1,35 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
+namespace PetjadesApi.Services;
+
+public class CloudinaryService
+{
+    private readonly Cloudinary _cloudinary;
+
+    public CloudinaryService(IConfiguration config)
+    {
+        var account = new Account(
+            config["CLOUDINARY_CLOUD_NAME"],
+            config["CLOUDINARY_API_KEY"],
+            config["CLOUDINARY_API_SECRET"]
+        );
+
+        _cloudinary = new Cloudinary(account);
+    }
+
+    public async Task<string> UploadImageAsync(IFormFile file)
+    {
+        using var stream = file.OpenReadStream();
+
+        var uploadParams = new ImageUploadParams
+        {
+            File = new FileDescription(file.FileName, stream),
+            Folder = "animals"
+        };
+
+        var result = await _cloudinary.UploadAsync(uploadParams);
+
+        return result.SecureUrl.AbsoluteUri;
+    }
+}
