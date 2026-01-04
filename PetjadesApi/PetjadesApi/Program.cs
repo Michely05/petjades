482838c5f0d2535093cff94fb82a8825a55c7999
[Michely Paredes Galeano] <[michelyparedes@gmail.com]>
2026-01-04 18:18:54 +0100

    create ResendEmailService

--- PetjadesApi/PetjadesApi/Program.cs
+++ PetjadesApi/PetjadesApi/Program.cs
@@ -45,7 +45,8 @@ builder.Services.AddScoped<IAnimalService, AnimalService>();
 builder.Services.AddScoped<IRequestRepository, RequestRepository>();
 builder.Services.AddScoped<IRequestService, RequestService>();
 
-builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<IEmailService, ResendEmailService>();
 
 builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
 builder.Services.AddScoped<IAppointmentService, AppointmentService>();
